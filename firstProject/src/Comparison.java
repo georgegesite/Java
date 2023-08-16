34410cce5d33a5f15c8de917653903899b3ad13c
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-16 15:10:12 +0800

    10, 11
    Comparison

---
+++ firstProject/src/Comparison.java
@@ -0,0 +1,12 @@
+public class Comparison {
+    public static void main (String[] args){
+        double a = 1.5;
+        double b = 2.5;
+        double c = 3;
+        double d = 5;
+
+        boolean try1 = a==b && c<d;
+
+        System.out.println(try1);
+    }
+}
--- firstProject/src/MyScanner.java
+++ firstProject/src/MyScanner.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class MyScanner {
     public static void main(String[] args){
-        // String str = "1234567890";
-        Scanner scan = new Scanner(System.in);
-        String txt = scan.next();
-
-        System.out.println(txt);
+        try (// Scanner scan = new Scanner(System.in);
+                // String txt = scan.next();
+                // System.out.println(txt);
+        Scanner scan = new Scanner(System.in)) {
+            boolean present = scan.nextBoolean();
+            if (present){
+                System.out.println("im present");
+            }
+            else{
+                System.out.println("not present");
+            }
+        }
+        
     }
 }
