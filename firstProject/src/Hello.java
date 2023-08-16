66447c237967ecd84ddd55735afb698565f9caae
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-16 14:28:02 +0800

    5,6,7
    Data Types, Variables and Basic Math operations

--- firstProject/src/Hello.java
+++ firstProject/src/Hello.java
@@ -1,6 +1,19 @@
 public class Hello {
     public static void  main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("My name is George");
+        // System.out.println("Hello World");
+        // System.out.println("My name is George");
+
+        long number = 10;
+        long number2 = 30;
+
+        long number3 = number + number2;
+
+        System.out.println(number3);
+
+        String name = "George";
+        
+        System.out.print(name);
+
+    
     }
 }
