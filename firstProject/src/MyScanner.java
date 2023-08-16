eb81a54ff13e3ee45824fde7397d4284e5b4f2fc
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-16 14:47:22 +0800

    8
    Scanner and string

---
+++ firstProject/src/MyScanner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MyScanner {
+    public static void main(String[] args){
+        // String str = "1234567890";
+        Scanner scan = new Scanner(System.in);
+        String txt = scan.next();
+
+        System.out.println(txt);
+    }
+}
