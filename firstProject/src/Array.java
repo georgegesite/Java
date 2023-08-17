29c49a596312939133bdf76e6472954d94cd710c
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-17 08:17:15 +0800

    Create Array.java
    Basic Data Storage

---
+++ firstProject/src/Array.java
@@ -0,0 +1,21 @@
+public class Array {
+    public static void main (String [] args)
+    {
+        double [] Data = new double[4];
+        Data[0] = 5.5;
+        Data[1] = 3.5;
+        Data[2] = 4.5;
+        Data[3] = 5.5;
+        
+
+        if(Data[3] > Data[0])
+        {
+            System.out.print("True");
+        }
+        else{
+            System.out.println("False");
+        }
+        }
+
+    }
+
