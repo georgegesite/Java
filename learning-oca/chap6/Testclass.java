d57f9745ff2a7616cc5bbad17417dd7e5eb80a9e
George Gesite <georgesite33@gmail.com>
2024-11-23 21:55:59 +0800

    Create Testclass.java

---
+++ learning-oca/chap6/Testclass.java
@@ -0,0 +1,11 @@
+public class Testclass {
+
+    public static void main(String[] args) {
+        try {
+            RuntimeException re = null;
+            throw re;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
