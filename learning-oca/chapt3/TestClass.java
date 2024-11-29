2918bdd58b3ac66653fc2cfef876db7a30c7c698
George Gesite <georgesite33@gmail.com>
2024-11-29 14:23:32 +0800

    Update TestClass.java

--- learning-oca/chapt3/TestClass.java
+++ learning-oca/chapt3/TestClass.java
@@ -1,13 +1,18 @@
 public class TestClass {
-    public static void main(String[] args) {
-        new TestClass().sayHello();
-    } // 1
+    char c;
 
-    public static void sayHello() {
-        System.out.println("Static Hello World");
-    } // 2
+    public void m1() {
+        char[] cA = { 'a', 'b' };
+        m2(c, cA);
+        System.out.println(((int) c) + "," + cA[1]);
+    }
 
-    public void sayHello() {
-        System.out.println("Hello World ");
-    } // 3
+    public void m2(char c, char[] cA) {
+        c = 'b';
+        cA[1] = cA[0] = 'm';
+    }
+
+    public static void main(String args[]) {
+        new TestClass().m1();
+    }
 }
